7afb34405fddf8d3fbefd1bbeab6d911d36342f7
MarkCnw <chinnawong554@gmail.com>
2025-02-19 20:07:31 +0700

    Initialize Next.js project with essential configuration and components

---
+++ my-app/app/page.js
@@ -0,0 +1,11 @@
+import { Button } from "@/components/UI/button"
+import Image from "next/image"
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button>subsets</Button>
+    </div>
+  )
+}
